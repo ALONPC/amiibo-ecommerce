c78b3b4252a0ca4f6a4c9a06eb71c6b8f814fd84
ALONPC <alon.berrios@gmail.com>
2020-07-05 05:08:06 -0400

    first commit

---
+++ src/components/AmiiboCard.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import { IconButton } from "@material-ui/core";
+
+export const AmiiboCard = ({ amiibo }) => {
+  console.log("AmiiboCard -> amiibo", amiibo);
+  const { character, image } = amiibo;
+  const useStyles = makeStyles((theme) => ({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      padding: "24px",
+    },
+    cardMedia: {
+      paddingTop: "56.25%", // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography gutterBottom variant="h5" component="h2">
+        {character}
+      </Typography>
+      <CardMedia
+        className={classes.cardMedia}
+        image={image}
+        title="Image title"
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography>
+          This is a media card. You can use this section to describe the
+          content.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          className={classes.button}
+          startIcon={<AddShoppingCartIcon />}>
+          Agregar al carrito
+        </Button>
+        {/* <IconButton
+          children={<AddShoppingCartIcon></AddShoppingCartIcon>}
+          size="small"
+          color="primary"></IconButton> */}
+      </CardActions>
+    </Card>
+  );
+};
---
+++ src/components/AmiiboList.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+import axios from "axios";
+import { AmiiboCard } from "./AmiiboCard";
+
+export const AmiiboList = () => {
+  const [amiibos, setAmiibos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log("ListAmiibo -> amiibos", amiibos);
+  console.log("ListAmiibo -> loading", loading);
+
+  useEffect(() => {
+    getAmiibos();
+  }, []);
+
+  const getAmiibos = async () => {
+    const amiibos = await axios
+      .get("https://www.amiiboapi.com/api/amiibo")
+      .then((res) => {
+        setLoading(false);
+        return res.data.amiibo;
+      });
+    const filteredAmiibos = amiibos.filter(
+      (amiibo) => amiibo.amiiboSeries === "Super Smash Bros."
+    );
+    setAmiibos(filteredAmiibos);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    cardMedia: {
+      paddingTop: "56.25%", // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.cardGrid} maxWidth="lg">
+      <Grid container spacing={4}>
+        {amiibos.map((amiibo) => (
+          <Grid item xs={12} sm={6} md={3}>
+            <AmiiboCard amiibo={amiibo}></AmiiboCard>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
