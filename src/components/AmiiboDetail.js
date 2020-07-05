94d8803a6c17fb5589456585a2a31cbd920672bd
ALONPC <alon.berrios@gmail.com>
2020-07-05 07:11:01 -0400

    favicon and some style improved

---
+++ src/components/AmiiboDetail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AmiiboDetail = () => {
+  return <div></div>;
+};
--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,17 +1,10 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
 import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 
 import { AmiiboList } from "./AmiiboList";
@@ -21,7 +14,7 @@ const Copyright = () => {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Amiibo Store
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -124,7 +117,7 @@ export const AppLayout = () => {
           align="center"
           color="textSecondary"
           component="p">
-          Something here to give the footer a purpose!
+          It's a me! Mario!
         </Typography>
         <Copyright />
       </footer>
